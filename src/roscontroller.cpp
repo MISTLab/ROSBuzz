149abb9d532f8b2a7a49051d133609d2cf3b14cf
David St-Onge <david.st-onge@polymtl.ca>
2017-01-25 21:10:07 -0500

    fix ROS std msgs

--- src/roscontroller.cpp
+++ src/roscontroller.cpp
@@ -37,12 +37,14 @@ namespace rosbzz_node{
   			{
       				/*Update neighbors position inside Buzz*/
      				buzz_utility::neighbour_pos_callback(neighbours_pos_map);
-				
+				auto current_time = ros::Time::now();
 				map< int, buzz_utility::Pos_struct >::iterator it;
 				rosbuzz::neigh_pos neigh_pos_array; //neigh_pos_array.clear(); 
 				for (it=raw_neighbours_pos_map.begin(); it!=raw_neighbours_pos_map.end(); ++it){
 					sensor_msgs::NavSatFix neigh_tmp;
 					//cout<<"iterator it val: "<< it-> first << " After convertion: " <<(uint8_t) buzz_utility::get_rid_uint8compac(it->first)<<endl;				
+					neigh_tmp.header.stamp = current_time;
+					neigh_tmp.header.frame_id = "/world";
 					neigh_tmp.position_covariance_type=it->first; //custom robot id storage
                         		neigh_tmp.longitude=(it->second).x;
                         		neigh_tmp.latitude=(it->second).y;
