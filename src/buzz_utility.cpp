84b48de33ea84cd7e3c2e29188e52ffe3af30a6f
vivek-shankar <robovivek@gmail.com>
2017-01-27 09:42:42 -0500

    debug

--- src/buzz_utility.cpp
+++ src/buzz_utility.cpp
@@ -137,6 +137,7 @@ namespace buzz_utility{
       		   tot += sizeof(uint8_t);
 		   /*Append updater msg size*/
 		   *(uint16_t*)(buff_send + tot) = *(uint16_t*) (getupdate_out_msg_size());
+			fprintf(stdout,"Updater sent msg size : %i \n", (int)*(uint16_t*) (getupdate_out_msg_size()));
       		   tot += sizeof(uint16_t);
 		   /*Append updater msgs*/   	
     		   memcpy(buff_send + tot, (uint8_t*)(getupdater_out_msg()), updater_msgSize);
