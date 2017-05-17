8c3771a4477ea172a0e87105ee159fa71a2d2d1b
vivek-shankar <robovivek@gmail.com>
2017-05-17 14:02:08 -0400

    update log file correction

--- src/buzz_update.cpp
+++ src/buzz_update.cpp
@@ -419,7 +419,7 @@ void collect_data(std::ofstream &logger){
 	double time_spent =   (t2.tv_sec - t1.tv_sec) * 1000.0; //(double)(end - begin) / CLOCKS_PER_SEC;
 	time_spent += (t2.tv_usec - t1.tv_usec) / 1000.0;
 	//int bytecodesize=(int);
-	logger<<(int)no_of_robot<<","<<neigh<<","<<(double)time_spent<<","<<(int)timer_steps<<","<<*(size_t*)updater->bcode_size<<","<<(int)no_of_robot<<","<<*(uint8_t*)updater->update_no;
+	logger<<(int)no_of_robot<<","<<neigh<<","<<(double)time_spent<<","<<(int)timer_steps<<","<<*(size_t*)updater->bcode_size<<","<<(int)*(uint8_t*)updater->update_no;
 	//fprintf(stdout,"Data logger Info : %i , %i , %f , %ld , %i , %d \n",(int)no_of_robot,neigh,time_spent,*(size_t*)updater->bcode_size,(int)no_of_robot,*(uint8_t*)updater->update_no);
 	//FILE *Fileptr;
 	//Fileptr=fopen("/home/ubuntu/ROS_WS/update_logger.csv", "a");
