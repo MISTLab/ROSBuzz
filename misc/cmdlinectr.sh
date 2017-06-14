cf35d9696b27f0d206576c7deaee24756683cef0
dave <david.st-onge@polymtl.ca>
2017-06-14 13:18:00 -0400

    fix obstacle detection

--- misc/cmdlinectr.sh
+++ misc/cmdlinectr.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 function takeoff {
-	rosservice call /buzzcmd 0 22 0 0 0 0 0 0 0 0
+	rosservice call $1/buzzcmd 0 22 0 0 0 0 0 0 0 0
 }
 function land {
-	rosservice call /buzzcmd 0 21 0 0 0 0 0 0 0 0
+	rosservice call $1/buzzcmd 0 21 0 0 0 0 0 0 0 0
 }
 function arm {
-	rosservice call /buzzcmd 0 400 0 1 0 0 0 0 0 0
+	rosservice call $1/buzzcmd 0 400 0 1 0 0 0 0 0 0
 }
 function disarm {
-	rosservice call /buzzcmd 0 400 0 0 0 0 0 0 0 0
+	rosservice call $1/buzzcmd 0 400 0 0 0 0 0 0 0 0
 }
 function record {
-	rosbag record /flight_status /global_position /users_pos /dji_sdk/local_position /neighbours_pos /power_status /guidance/obstacle_distance /guidance/front/depth/image_rect/compressedDepth /guidance/right/depth/image_rect/compressedDepth /guidance/front/depth/points /guidance/right/depth/points /guidance/front/right/image_rect/compressed /guidance/front/left/image_rect/compressed /guidance/right/right/image_rect/compressed /guidance/right/left/image_rect/compressed /guidance/front/left/camera_info /guidance/front/right/camera_info /guidance/right/right/camera_info /guidance/right/left/camera_info
+	rosbag record $1/flight_status $1/global_position $1/users_pos $1/local_position $1/neighbours_pos /power_status /guidance/obstacle_distance /guidance/front/depth/image_rect/compressedDepth /guidance/right/depth/image_rect/compressedDepth /guidance/front/depth/points /guidance/right/depth/points /guidance/front/right/image_rect/compressed /guidance/front/left/image_rect/compressed /guidance/right/right/image_rect/compressed /guidance/right/left/image_rect/compressed /guidance/front/left/camera_info /guidance/front/right/camera_info /guidance/right/right/camera_info /guidance/right/left/camera_info
 }
 function clean {
 	sudo rm /var/log/upstart/robot*
